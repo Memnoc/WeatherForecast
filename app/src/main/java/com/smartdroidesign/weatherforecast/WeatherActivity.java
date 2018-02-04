218655e1785356b2bcfbc70c04efdbc747d8b7ae
Memnoc <smartdroidesign@gmail.com>
2018-02-04 21:07:53 +0000

    - implemented LocationServices - working

--- app/src/main/java/com/smartdroidesign/weatherforecast/WeatherActivity.java
+++ app/src/main/java/com/smartdroidesign/weatherforecast/WeatherActivity.java
@@ -1,12 +1,19 @@
 package com.smartdroidesign.weatherforecast;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
+
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
@@ -25,6 +32,7 @@ public class WeatherActivity extends AppCompatActivity implements GoogleApiClien
     final String API_TOKEN = "&APPID=3a834efe29e097a21b0d01f86439e5ee";
 
     private GoogleApiClient mGoogleApiClient;
+    private final int PERMISSION_LOCATION = 111;
 
 
 
@@ -44,10 +52,13 @@ public class WeatherActivity extends AppCompatActivity implements GoogleApiClien
 
 
 
-    public void downloadWeatherData(){
-        final String url = BASE_URL + URL_COORD + URL_UNITS + API_TOKEN;
+    public void downloadWeatherData(Location location){
+        final String fullCords = URL_COORD + location.getLatitude() + "&lon=" + location.getLongitude();
+        final String url = BASE_URL + fullCords + URL_UNITS + API_TOKEN;
         // "https://swapi.co/api/people/1/" StarWars API
         // "https://pokeapi.co/api/v2/pokemon/" Pokemon API
+
+        Log.v("FUN2", "URL: " + url);
 
 
         final JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
@@ -61,7 +72,6 @@ public class WeatherActivity extends AppCompatActivity implements GoogleApiClien
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-
                 Log.v("FUN", "Err: " + error.getLocalizedMessage());
 
             }
@@ -74,31 +84,18 @@ public class WeatherActivity extends AppCompatActivity implements GoogleApiClien
 
     @Override
     public void onLocationChanged(Location location) {
-
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
+        downloadWeatherData(location);
 
     }
 
     @Override
     public void onConnected(@Nullable Bundle bundle) {
 
-    }
-
-    @Override
-    public void onConnectionSuspended(int i) {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSION_LOCATION);
+        } else {
+            startLocationServices();
+        }
 
     }
 
@@ -106,4 +103,38 @@ public class WeatherActivity extends AppCompatActivity implements GoogleApiClien
     public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    public void startLocationServices(){
+        try {
+            LocationRequest req = LocationRequest.create().setPriority(LocationRequest.PRIORITY_LOW_POWER);
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,req,this);
+        } catch (SecurityException exception) {
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode){
+            case PERMISSION_LOCATION: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    startLocationServices();
+                } else {
+                    // show a pop up dialog stating permission has been denied
+                    Toast.makeText(this, "I cannot run your location yo", Toast.LENGTH_LONG).show();
+
+                }
+            }
+        }
+    }
+
+
 }
