53974fa04b072130e15f50f000b65f4411e0d330
Memnoc <smartdroidesign@gmail.com>
2018-02-04 20:01:22 +0000

    - implemented GoogleAPI client for location

--- app/src/main/java/com/smartdroidesign/weatherforecast/WeatherActivity.java
+++ app/src/main/java/com/smartdroidesign/weatherforecast/WeatherActivity.java
@@ -1,24 +1,31 @@
 package com.smartdroidesign.weatherforecast;
-
+import com.google.android.gms.location.LocationServices;
+import android.location.Location;
+import android.location.LocationListener;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.json.JSONObject;
 
-public class WeatherActivity extends AppCompatActivity {
+public class WeatherActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks, LocationListener {
 
     final String BASE_URL =  "http://api.openweathermap.org/data/2.5/forecast";
-    final String URL_COORD = "/?lat=40.925471&lon=9.4944273";
+    final String URL_COORD = "/?lat=";//40.925471&lon=9.4944273";
     final String URL_UNITS = "&units=metric";
     final String API_TOKEN = "&APPID=3a834efe29e097a21b0d01f86439e5ee";
-// 40.925471,9.4944273
+
+    private GoogleApiClient mGoogleApiClient;
+
 
 
     @Override
@@ -26,6 +33,18 @@ public class WeatherActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
+        mGoogleApiClient  =new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .enableAutoManage(this, this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+    }
+
+
+
+    public void downloadWeatherData(){
         final String url = BASE_URL + URL_COORD + URL_UNITS + API_TOKEN;
         // "https://swapi.co/api/people/1/" StarWars API
         // "https://pokeapi.co/api/v2/pokemon/" Pokemon API
@@ -50,6 +69,41 @@ public class WeatherActivity extends AppCompatActivity {
 
         Volley.newRequestQueue(this).add(jsonRequest);
 
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
 
     }
 }
